d6563190cad3cc69efed9fe055a754c31a592e30
limshixun <limshixun1106@gmail.com>
2023-03-06 22:22:36 +0800

    combined setter for newTora,newAsda,newToda,newLda into one setNewParameter(). Add toString method

--- src/Model/LogicalRunway.java
+++ src/Model/LogicalRunway.java
@@ -26,67 +26,93 @@ public class LogicalRunway {
         this.lda = lda;
     }
 
+    public void setNewParameter(double newTora,double newAsda, double newToda, double newLda){
+        this.newTora = newTora;
+        this.newAsda = newAsda;
+        this.newToda = newToda;
+        this.newLda  = newLda ;
+    }
+
+    @Override
+    public String toString() {
+        return "LogicalRunway{" +
+                "designator='" + designator + '\'' +
+                ", tora=" + tora +
+                ", toda=" + toda +
+                ", asda=" + asda +
+                ", lda=" + lda +
+                ", newTora=" + newTora +
+                ", newToda=" + newToda +
+                ", newAsda=" + newAsda +
+                ", newLda=" + newLda +
+                '}';
+    }
+
     public String getDesignator() {
         return designator;
     }
+
     public void setDesignator(String designator) {
         this.designator = designator;
     }
+
     public double getTora() {
         return tora;
     }
+
     public void setTora(double tora) {
         this.tora = tora;
     }
+
     public double getToda() {
         return toda;
     }
+
     public void setToda(double toda) {
         this.toda = toda;
     }
+
     public double getAsda() {
         return asda;
     }
+
     public void setAsda(double asda) {
         this.asda = asda;
     }
+
     public double getLda() {
         return lda;
     }
+
     public void setLda(double lda) {
         this.lda = lda;
     }
+
     public double getClearway() {
         return getToda() - getTora();
     }
+
     public double getStopway() {
         return getTora() - getAsda();
     }
+
     public double getDisplacedThreshold() {
         return getTora() - getLda();
     }
+
     public double getNewTora() {
         return newTora;
     }
-    public void setNewTora(double newTora) {
-        this.newTora = newTora;
-    }
+
     public double getNewToda() {
         return newToda;
     }
-    public void setNewToda(double newToda) {
-        this.newToda = newToda;
-    }
+
     public double getNewAsda() {
         return newAsda;
     }
-    public void setNewAsda(double newAsda) {
-        this.newAsda = newAsda;
-    }
+
     public double getNewLda() {
         return newLda;
-    }
-    public void setNewLda(double newLda) {
-        this.newLda = newLda;
     }
 }
