91e8dbde576ec6e65ecdbefab5f85cbd72ac1f4c
limshixun <limshixun1106@gmail.com>
2023-03-06 18:46:28 +0800

    Add Calculator.java and variables. Rename Runway.java -> PhysicalRunway.java

---
+++ src/Model/Calculator.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class Calculator {
+    //als and tocs
+    private final static double slopeRatio = 50;
+    private final static double resa = 240;
+    private final static double blastProtection = 300;
+    private final static double stripEnd = 60;
+
+    //calculate Take-off Away and Landing Over
+    public static void calcTALO(Obstacle obstacle, LogicalRunway runways){
+
+    }
+
+    //calculate Take-off Towards AND Landing Towards
+    public static void calcTTLT(){
+
+    }
+}
