658c604086b75459e9e52fe099ae926b44ab695e
brodude11 <nyohith@gmail.com>
2023-03-06 23:33:37 +0800

    Added constructors, getters and setters

--- src/Model/Airport.java
+++ src/Model/Airport.java
@@ -1,4 +1,29 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Airport {
+    private String name;
+    private ArrayList<Runway> runwayList;
+
+    public Airport(String name, ArrayList<Runway> runwayList) {
+        this.name = name;
+        this.runwayList = runwayList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Runway> getRunwayList() {
+        return runwayList;
+    }
+
+    public void setRunwayList(ArrayList<Runway> runwayList) {
+        this.runwayList = runwayList;
+    }
 }
--- src/Model/Obstacle.java
+++ src/Model/Obstacle.java
@@ -1,4 +1,63 @@
 package Model;
 
 public class Obstacle {
+    private String name, nearestDesignator;
+    private double height, width, distFCenterLine, distFThreshold;
+
+    public Obstacle(String name, double height, double width, double distFCenterLine, String nearestDesignator, double distFThreshold) {
+        this.name = name;
+        this.height = height;
+        this.width = width;
+        this.distFCenterLine = distFCenterLine;
+        this.nearestDesignator = nearestDesignator;
+        this.distFThreshold = distFThreshold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNearestDesignator() {
+        return nearestDesignator;
+    }
+
+    public void setNearestDesignator(String nearestDesignator) {
+        this.nearestDesignator = nearestDesignator;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getDistFCenterLine() {
+        return distFCenterLine;
+    }
+
+    public void setDistFCenterLine(double distFCenterLine) {
+        this.distFCenterLine = distFCenterLine;
+    }
+
+    public double getDistFThreshold() {
+        return distFThreshold;
+    }
+
+    public void setDistFThreshold(double distFThreshold) {
+        this.distFThreshold = distFThreshold;
+    }
 }
