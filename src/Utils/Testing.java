22b2c59af766a5012e49a8a04033358a80be25a8
b-knd <angjingru@gmail.com>
2023-03-07 16:44:30 +0800

    Commit

--- src/Utils/Testing.java
+++ src/Utils/Testing.java
@@ -1,3 +1,5 @@
+package Utils;
+
 public class Testing {
     public static void main(String[] args) {
         //jr successful commit
--- src/View/Main.java
+++ src/View/Main.java
@@ -1,5 +1,6 @@
 package View;
 
+import Controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.*;
 import javafx.scene.Parent;
