4a805c0b88b60b0aba2d10c4d244c53f3dd3be51
limshixun <limshixun1106@gmail.com>
2023-03-07 12:29:23 +0800

    add variables, getter, setter, and toString() to PhysicalRunway.java

--- src/Model/PhysicalRunway.java
+++ src/Model/PhysicalRunway.java
@@ -1,5 +1,37 @@
 package Model;
 
-public class PhysicalRunway{
+import javafx.collections.ObservableList;
 
+public class PhysicalRunway{
+    private String name;
+    private ObservableList<LogicalRunway> logicalRunways;
+
+    @Override
+    public String toString() {
+        return "PhysicalRunway{" +
+                "name='" + name + '\'' +
+                "\nlogicalRunways=" + logicalRunways +
+                '}';
+    }
+
+    public PhysicalRunway(String name, ObservableList<LogicalRunway> logicalRunways){
+        this.name = name;
+        this.logicalRunways = logicalRunways;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ObservableList<LogicalRunway> getLogicalRunways() {
+        return logicalRunways;
+    }
+
+    public void setLogicalRunways(ObservableList<LogicalRunway> logicalRunways) {
+        this.logicalRunways = logicalRunways;
+    }
 }
