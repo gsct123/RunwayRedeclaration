2b5e29fabd87ab035eca4b423dee5f5eefa8996d
b-knd <angjingru@gmail.com>
2023-03-07 09:53:44 +0800

    Updated fxml

--- src/View/Main.java
+++ src/View/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-
+        //sdf
         Scene scene = new Scene(root);
 
         stage.setTitle("SEG Runway Project");
