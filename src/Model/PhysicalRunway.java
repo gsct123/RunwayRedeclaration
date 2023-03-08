940a57ff97c57361e12b9edf3509b9a2d1b60d00
limshixun <limshixun1106@gmail.com>
2023-03-08 22:26:47 +0800

    Add Obstacle as variable of PhysicalRunway. getter and setter for obstacle and modify constructor

--- src/Model/PhysicalRunway.java
+++ src/Model/PhysicalRunway.java
@@ -5,18 +5,23 @@ import javafx.collections.ObservableList;
 public class PhysicalRunway{
     private String name;
     private ObservableList<LogicalRunway> logicalRunways;
+    //Obstacle is on a physical runway, one obstacle could affect all the logical runway in a physical runway.
+    //There could only be ONE obstacle according to the requirement
+    private Obstacle obstacle;
 
     @Override
     public String toString() {
         return "PhysicalRunway{" +
                 "name='" + name + '\'' +
                 "\nlogicalRunways=" + logicalRunways +
+                "\nobstacle=" + obstacle +
                 '}';
     }
 
-    public PhysicalRunway(String name, ObservableList<LogicalRunway> logicalRunways){
+    public PhysicalRunway(String name, ObservableList<LogicalRunway> logicalRunways, Obstacle obstacle){
         this.name = name;
         this.logicalRunways = logicalRunways;
+        this.obstacle = obstacle;
     }
 
     public String getName() {
@@ -33,5 +38,13 @@ public class PhysicalRunway{
 
     public void setLogicalRunways(ObservableList<LogicalRunway> logicalRunways) {
         this.logicalRunways = logicalRunways;
+    }
+
+    public Obstacle getObstacle() {
+        return obstacle;
+    }
+
+    public void setObstacle(Obstacle obstacle) {
+        this.obstacle = obstacle;
     }
 }
