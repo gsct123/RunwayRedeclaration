11f414b3b822517b891376272990b5764ed2b93b
b-knd <angjingru@gmail.com>
2023-03-08 18:37:14 +0800

    Commit

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,24 +1,113 @@
 package Controller;
 
+import Model.Airport;
+import Model.LogicalRunway;
+import Model.PhysicalRunway;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.util.stream.Collectors;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainController {
     private ObservableList<String> items = FXCollections.observableArrayList();
 
     private MenuButton airportMenu;
 
-    public void initialize() {
-        items.addListener((ListChangeListener<String>) change -> {
-            airportMenu.getItems().setAll(
-                    items.stream().map(MenuItem::new).collect(Collectors.toList())
-            );
-        });
+    public static void main(String[] args) throws Exception {
+        List<Airport> airports = helperReader("src/Data/airports.xml");
+        System.out.println(airports);
     }
+
+    public void initialize() {
+    }
+
+    public static List<Airport> helperReader(String file) throws Exception {
+        // Create a DocumentBuilder
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        // Parse the XML file into a Document
+        Document doc = docBuilder.parse(file);
+
+        // Get a NodeList of all airport elements
+        NodeList airportElements = doc.getElementsByTagName("airport");
+
+        // Create a list to hold the airports
+        List<Airport> airports = new ArrayList<>();
+
+        // Loop over each airport element and create an Airport object
+        for (int i = 0; i < airportElements.getLength(); i++) {
+            Node airportNode = airportElements.item(i);
+
+            if (airportNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element airportElement = (Element) airportNode;
+
+                // Get the airport name
+                String airportName = airportElement.getElementsByTagName("name").item(0).getTextContent();
+
+                // Create a list to hold the physical runways
+                ObservableList<PhysicalRunway> physicalRunways = FXCollections.observableArrayList();
+
+                // Get a NodeList of all physical runway elements for the current airport
+                NodeList physRunwayElements = airportElement.getElementsByTagName("physicalRunway");
+
+                // Loop over each physical runway element and create a PhysicalRunway object
+                for (int j = 0; j < physRunwayElements.getLength(); j++) {
+                    Node physRunwayNode = physRunwayElements.item(j);
+
+                    if (physRunwayNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element physRunwayElement = (Element) physRunwayNode;
+
+                        // Get the physical runway name
+                        String physRunwayName = physRunwayElement.getAttribute("name");
+
+                        // Create a list to hold the logical runways
+                        ObservableList<LogicalRunway> logicalRunways = FXCollections.observableArrayList();
+
+                        // Get a NodeList of all logical runway elements for the current physical runway
+                        NodeList logRunwayElements = physRunwayElement.getElementsByTagName("logicalRunway");
+
+                        // Loop over each logical runway element and create a LogicalRunway object
+                        for (int k = 0; k < logRunwayElements.getLength(); k++) {
+                            Node logRunwayNode = logRunwayElements.item(k);
+
+                            if (logRunwayNode.getNodeType() == Node.ELEMENT_NODE) {
+                                Element logRunwayElement = (Element) logRunwayNode;
+
+                                // Get the logical runway designator and dimensions
+                                String designator = logRunwayElement.getAttribute("designator");
+                                double tora = Double.parseDouble(logRunwayElement.getAttribute("tora"));
+                                double toda = Double.parseDouble(logRunwayElement.getAttribute("toda"));
+                                double asda = Double.parseDouble(logRunwayElement.getAttribute("asda"));
+                                double lda = Double.parseDouble(logRunwayElement.getAttribute("lda"));
+
+                                // Create a new LogicalRunway object and add it to the list of logical runways
+                                LogicalRunway logicalRunway = new LogicalRunway(designator, tora, toda, asda, lda);
+                                logicalRunways.add(logicalRunway);
+                            }
+                        }
+
+                        // Create a new PhysicalRunway object with the logical runways and add it to the list of physical runways
+                        PhysicalRunway physicalRunway = new PhysicalRunway(physRunwayName, logicalRunways);
+                        physicalRunways.add(physicalRunway);
+                    }
+                }
+
+                // Create a new Airport object with the physical runways and add it to the list of airports
+                airports.add(new Airport(airportName, physicalRunways));
+            }
+        }
+        return airports;
+    }
+
 
 }
